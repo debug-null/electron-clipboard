6c0f171dd8c969dd355da3d431b21b0acc069664
yanbin <sybsss@sina.cn>
2020-12-07 22:40:07 +0800

    feat: add iohook mac

--- src/electron/main-process/sql/index.js
+++ src/electron/main-process/sql/index.js
@@ -1,6 +1,7 @@
 const { app } = require("electron");
 const ioHook = require("iohook");
 import DataBase from "@/sql/index.js";
+import { getClipboardData } from "@/utils/clipboard.js";
 import { continuousDetect } from "@/utils/index";
 
 app.on("browser-window-created", () => {
@@ -12,24 +13,45 @@ app.on("browser-window-created", () => {
         "content"	TEXT NOT NULL,
         "source"	TEXT,
         PRIMARY KEY("id" AUTOINCREMENT)
-  )`
+  )`,
   ];
   Sqlite.init({ sql, name: "superCopy" });
 
   ioHook.start();
   //初始化闭包
   let continuousDetectFn = continuousDetect();
-  ioHook.on("keypress", event => {
+  ioHook.on("keypress", (event) => {
     let rawcode = event.rawcode;
-    switch (rawcode) {
-      case 86:
-        if (event.ctrlKey) {
-          continuousDetectFn(() => {
-            console.log("连续触发ctrl+v");
-          });
-        }
+    let platform = process.platform;
+    if (platform === "win32") {
+      switch (rawcode) {
+        case 86:
+          if (event.ctrlKey) {
+            continuousDetectFn(() => {
+              console.log("Win-连续触发ctrl+v");
+            });
+          }
 
-        break;
+          break;
+      }
+    }
+
+    if (platform === "darwin") {
+      switch (rawcode) {
+        case 9:
+          if (event.metaKey) {
+            continuousDetectFn(async () => {
+              console.log("MAC-连续触发ctrl+v");
+              let text = await getClipboardData();
+              console.log(
+                "🚀 ~ file: index.js ~ line 49 ~ continuousDetectFn ~ text",
+                text
+              );
+            });
+          }
+
+          break;
+      }
     }
   });
 });
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,10 @@
 export function continuousDetect() {
-  var count = 0; // 连续点击次数
+  var count = 1; // 连续点击次数
   var lastTime = new Date().getTime(); //上次点击时间
   var timer = null;
 
   return callback => {
-    var waitTime = 1000;
+    var waitTime = 600;
     var currentTime = new Date().getTime();
     //计算2次点击的间隔时间
     count = currentTime - lastTime < waitTime ? count + 1 : 1;
@@ -13,7 +13,7 @@ export function continuousDetect() {
 
     timer = setTimeout(() => {
       clearTimeout(timer);
-      if (count > 2) {
+      if (count >= 2) {
         callback();
       }
     }, waitTime);
